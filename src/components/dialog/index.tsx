b3da84c27ac4898d8302139b88c3ac1abf4e5258
Bayathy <misosoup0321@icloud.com>
2023-09-28 22:37:00 +0900

    feat: 画像アップロード機能の追加

---
+++ src/components/dialog/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import type { FC } from "react";
+
+import * as RadixDialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
+
+export type DialogProps = {
+  trigger: React.ReactNode;
+  children: React.ReactNode;
+};
+
+export const Dialog: FC<DialogProps> = ({ trigger, children }) => {
+  return (
+    <RadixDialog.Root>
+      <RadixDialog.Trigger asChild>{trigger}</RadixDialog.Trigger>
+      <RadixDialog.Overlay className="fixed inset-0 z-40 bg-black opacity-30" />
+      <RadixDialog.Content className="fixed left-1/2 top-1/2 z-50 min-h-[384px] w-full max-w-sm -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white px-4 pt-8 shadow-lg md:max-w-2xl">
+        <RadixDialog.Close className={"absolute right-4 top-4"}>
+          <Cross2Icon />
+        </RadixDialog.Close>
+        {children}
+      </RadixDialog.Content>
+    </RadixDialog.Root>
+  );
+};
--- src/ux-domain/event/general/components/event-name-panel-card.tsx
+++ src/ux-domain/event/general/components/event-name-panel-card.tsx
@@ -1,6 +1,39 @@
 import { PanelCard } from "../../components/panel-card";
 
 import { Icon } from "@/components/Icon";
+import { Dialog } from "@/components/dialog";
+
+const EventNameEditDialog = () => {
+  return (
+    <Dialog
+      trigger={
+        <button aria-label="イベントの名前を演習する">
+          <Icon height={24} width={24} icon={"ri:edit-2-fill"} />
+        </button>
+      }
+    >
+      <div className={"flex flex-col gap-4"}>
+        <p className={"text-lg text-deepBlue"}>イベント名を変更する</p>
+        <form>
+          <label htmlFor="event-name">イベント名</label>
+          <input
+            id="event-name"
+            type="text"
+            className={"mt-2 w-full rounded-lg border-2 border-deepBlue"}
+          />
+          <button
+            className={
+              "absolute bottom-4 right-4 rounded-lg bg-deepBlue px-4 py-2 text-white"
+            }
+            type="submit"
+          >
+            変更
+          </button>
+        </form>
+      </div>
+    </Dialog>
+  );
+};
 
 export const EventNamePanelCard = () => {
   return (
@@ -8,7 +41,7 @@ export const EventNamePanelCard = () => {
       <p className={"text-lg text-deepBlue"}>イベント名</p>
       <div className={"flex items-center justify-between gap-2"}>
         <p className={"text-xl"}>This is event name</p>
-        <Icon height={24} width={24} icon={"ri:edit-2-fill"} />
+        <EventNameEditDialog />
       </div>
     </PanelCard>
   );
