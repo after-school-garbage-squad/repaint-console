ccd76f201853181b0c25cc3570dece1cc9ef1ae1
Bayathy <misosoup0321@icloud.com>
2023-10-03 07:22:04 +0900

    feat: dialogのレスポンシブ対応

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -15,7 +15,7 @@ export const Dialog: FC<DialogProps> = ({ trigger, children }) => {
     <RadixDialog.Root>
       <RadixDialog.Trigger asChild>{trigger}</RadixDialog.Trigger>
       <RadixDialog.Overlay className="fixed inset-0 z-40 bg-black opacity-30" />
-      <RadixDialog.Content className="fixed left-1/2 top-1/2 z-50 min-h-[384px] w-full max-w-sm -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white px-4 pt-8 shadow-lg md:max-w-2xl">
+      <RadixDialog.Content className="fixed left-1/2 top-1/2 z-50 min-h-[384px] w-[90%] max-w-sm  -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white px-4 pt-8 shadow-lg md:w-full md:max-w-2xl">
         <RadixDialog.Close className={"absolute right-4 top-4"}>
           <Cross2Icon />
         </RadixDialog.Close>
