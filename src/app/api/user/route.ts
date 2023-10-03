83e3e40226d4d294d7b57d653456bc331078b9f5
Bayathy <misosoup0321@icloud.com>
2023-10-04 05:05:45 +0900

    fix: api/user下のgetSessionの修正

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,16 +1,10 @@
-import { getSession } from "@auth0/nextjs-auth0";
 import { NextResponse } from "next/server";
-
-import type { NextRequest } from "next/server";
 
 export const fetchCache = "force-no-store";
 
-// eslint-disable-next-line no-console
-console.log("AUTH0_SECRET =", process.env.AUTH0_SECRET);
-
-export const GET = async (req: NextRequest) => {
-  const res = new NextResponse();
-  const session = await getSession(req, res);
+export const GET = async () => {
+  const { getSession } = await import("@auth0/nextjs-auth0");
+  const session = await getSession();
   if (!session?.idToken) return;
   const idToken = session.idToken;
   try {
