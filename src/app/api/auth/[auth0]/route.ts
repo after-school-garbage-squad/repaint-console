bae3a3aa2c9c2284283559d3bc32e29659c3abd7
Bayathy <misosoup0321@icloud.com>
2023-10-04 07:36:50 +0900

    fix: handleAuth修正漏れの対応

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -3,5 +3,5 @@ import type { NextRequest } from "next/server";
 export const GET = async (request: NextRequest, context: unknown) => {
   const { handleAuth } = await import("@auth0/nextjs-auth0");
 
-  return handleAuth({})(request, context);
+  return handleAuth()(request, context);
 };
