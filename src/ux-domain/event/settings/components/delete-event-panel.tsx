84b7d6742dcc7ef9bbbc218bb96940f826cbfff7
Bayathy <misosoup0321@icloud.com>
2023-10-04 17:36:03 +0900

    feat: イベント削除機能の追加

--- src/ux-domain/event/settings/components/delete-event-panel.tsx
+++ src/ux-domain/event/settings/components/delete-event-panel.tsx
@@ -1,26 +1,47 @@
 "use client";
 
+import { useState } from "react";
+
 import { Close } from "@radix-ui/react-dialog";
 import { useAtom } from "jotai";
+import { useRouter } from "next/navigation";
 
 import { PanelCard } from "../../components/panel-card";
 
 import { Dialog } from "@/components/dialog";
+import { getIdToken } from "@/domain/auth/api/get-id-token";
+import { deleteEvent } from "@/domain/event/api/delete-event";
 import { selectEventIdAtom } from "@/domain/event/store/atom";
 
 export const DeleteEventDialog = () => {
+  const router = useRouter();
+
   const [selectEventId] = useAtom(selectEventIdAtom);
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
+  const onSubmit = async () => {
+    try {
+      const idToken = await getIdToken();
+      await deleteEvent(idToken, selectEventId);
+
+      setIsDialogOpen(false);
+      router.push("/dashboard");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Dialog
+      open={isDialogOpen}
+      onOpenChange={() => setIsDialogOpen(!isDialogOpen)}
       trigger={
         <button
           className={"mt-2 rounded-lg bg-red px-4 py-2 text-white"}
-          aria-label="イベントを削除する"
-        >
+          aria-label="イベントを削除する">
           イベントを削除する
         </button>
-      }
-    >
+      }>
       <div className={"flex flex-col gap-4"}>
         <p className={"flex-1 text-lg text-deepBlue"}>
           イベントを削除しますか？
@@ -29,18 +50,14 @@ export const DeleteEventDialog = () => {
           <Close asChild>
             <button
               className={"flex-1 rounded-lg bg-deepBlue px-4 py-2 text-white"}
-              aria-label="イベントを削除をキャンセルする"
-            >
+              aria-label="イベントを削除をキャンセルする">
               キャンセル
             </button>
           </Close>
           <button
-            onSubmit={() => {
-              console.log(selectEventId);
-            }}
+            onClick={onSubmit}
             className={"flex-1 rounded-lg bg-red px-4 py-2 text-white"}
-            aria-label="イベントを削除を確定する"
-          >
+            aria-label="イベントを削除を確定する">
             削除する
           </button>
         </div>
@@ -51,7 +68,7 @@ export const DeleteEventDialog = () => {
 
 export const DeleteEventPanel = () => {
   return (
-    <PanelCard>
+    <PanelCard className={"flex items-center justify-between"}>
       <p className={"text-lg text-deepBlue"}>イベント削除</p>
       <DeleteEventDialog />
     </PanelCard>
