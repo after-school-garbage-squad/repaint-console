a03d192667a9b9d333bc7db0ffed6484ed49e096
Bayathy <misosoup0321@icloud.com>
2023-10-06 02:33:56 +0900

    feat: sessionをアップデートするようにした

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,8 +1,17 @@
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
-  const { getSession } = await import("@auth0/nextjs-auth0");
+import type { NextRequest } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  const res = NextResponse.next();
+  const { getSession, updateSession } = await import("@auth0/nextjs-auth0");
   const session = await getSession();
+
+  await updateSession(req, res, {
+    ...session,
+    user: { ...session?.user, name: "UPDATED" },
+  });
+
   if (!session?.idToken) {
     return NextResponse.json("can not get idToken from session", {
       status: 401,
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,19 @@
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 
-export default withMiddlewareAuthRequired({
-  returnTo: "/dashboard",
+import type { NextRequest } from "next/server";
+
+// const afterRefresh = async (session: Session) => {
+//   delete session.idToken;
+//   return session;
+// };
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default withMiddlewareAuthRequired(async (_req: NextRequest) => {
+  // const res = NextResponse.next();
+  // await getAccessToken(req, res, {
+  //   refresh: true,
+  //   afterRefresh,
+  // });
 });
 
 export const config = {
