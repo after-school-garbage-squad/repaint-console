19b826147be0657c3878a421b18cccab42f6f676
Bayathy <misosoup0321@icloud.com>
2023-10-14 02:07:16 +0900

    fix: dialogのパッチ

--- src/ux-domain/shared-ui/ErrorAlertDialog/error-alert-dialog.tsx
+++ src/ux-domain/shared-ui/ErrorAlertDialog/error-alert-dialog.tsx
@@ -6,8 +6,7 @@ import { useRouter } from "next/navigation";
 
 import { alertDialogStateAtom } from "./atom";
 
-import { TokenError } from "@/domain/auth/error";
-
+// TODO: このエラーハンドリングをしゅうせいする
 export const ErrorAlertDialog = () => {
   const [state, setState] = useAtom(alertDialogStateAtom);
   const router = useRouter();
@@ -18,20 +17,21 @@ export const ErrorAlertDialog = () => {
         <Dialog.Overlay className="fixed inset-0 z-40 bg-black opacity-30" />
         <Dialog.Content className="fixed left-1/2 top-1/2 z-50 max-h-[320px] w-[90%] max-w-sm -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white px-4 pb-4 pt-8 shadow-lg md:w-full md:max-w-2xl">
           <Dialog.Title className="text-lg font-bold text-red">
-            {state?.error instanceof TokenError
+            {/* {state?.error instanceof TokenError
               ? "ログインセッションが切れました。"
-              : "通信エラーが発生しました。"}
+              : "通信エラーが発生しました。"} */}
+            通信エラーが発生しました。
           </Dialog.Title>
-          <Dialog.Description className="mt-2 text-base">
+          {/* <Dialog.Description className="mt-2 text-base">
             <p>
               {state?.error instanceof TypeError
                 ? state.error.message
                 : "ログインセッションが切れました。再度ログインしてください。"}
             </p>
-          </Dialog.Description>
+          </Dialog.Description> */}
           <div className={"flex justify-end"}>
             <Dialog.Action asChild>
-              {state?.error instanceof TypeError ? (
+              {/* {state?.error instanceof TypeError ? (
                 <button
                   onClick={(e) => {
                     e.preventDefault();
@@ -52,7 +52,17 @@ export const ErrorAlertDialog = () => {
                 >
                   ログイン画面へ
                 </button>
-              )}
+              )} */}
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setState({ isOpen: !state?.isOpen, error: null });
+                  router.refresh();
+                }}
+                className="rounded-lg bg-deepBlue px-4 py-2 text-white"
+              >
+                閉じる
+              </button>
             </Dialog.Action>
           </div>
         </Dialog.Content>
